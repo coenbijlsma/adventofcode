b6f8ed28907d45c50c0ffeae0e5d9dbfa595bd2a
Coen Bijlsma <github@coenbijlsma.nl>
2015-12-10 20:13:49 +0100

    Solved day 4

---
+++ day04/index.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var crypto = require('crypto');
+var input = 'yzbqklnj';
+
+for (let i = 0; true; i++) {
+    let hash = crypto.createHash('md5');
+    hash.update(input + i, 'ascii');
+
+    let output = hash.digest('hex');
+    if (output.substr(0, 5) === '00000') {
+        console.log('Answer = ' + i + ' (' + output + ')');
+        break;
+    }
+}
